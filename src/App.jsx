f61377f71930ccc29d88e8ccc5cdaef1f781c1b1
itssolox <fksk7850@gmail.com>
2025-05-04 10:40:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ import { DataProvider } from './context/DataContext';
 import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Bins from './pages/Bins';
 import BinDetails from './pages/BinDetails';
 import Methods from './pages/Methods';
 import MethodDetails from './pages/MethodDetails';
 import Support from './pages/Support';
 import Admin from './pages/Admin';
-import Login from './pages/Login';
 import './styles/App.css';
 
 function App() {
@@ -34,12 +34,12 @@ function App() {
             <main>
               <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/bins" element={<Bins />} />
                 <Route path="/bin/:id" element={<BinDetails />} />
                 <Route path="/methods" element={<Methods />} />
                 <Route path="/methods/:id" element={<MethodDetails />} />
                 <Route path="/support" element={<Support />} />
                 <Route path="/admin" element={<Admin />} />
-                <Route path="/login" element={<Login />} />
               </Routes>
             </main>
             <ToastContainer position="bottom-right" />
